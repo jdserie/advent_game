78bea8585039a52625579ea817b9247ff0624557
Joshua Serie <serie1jd@gmail.com>
2019-03-14 12:51:35 -0400

    I used a pattern I found. not finished

---
+++ santa_code_game.py
@@ -0,0 +1,35 @@
+
+
+
+
+# def zero_up():
+#     if count > 0:
+
+count = 0
+zero = 1
+n = 1
+result = 0
+while n > 0: 
+    if zero == 1:
+        x = input("Enter r or l")
+        y = int(input("Enter a number"))
+        if x == 'r':
+            count += .5
+            if count != 0:
+                if count % 2 == 0:
+                    result = y + 1
+                else:
+                    
+            elif count ==0:
+            
+        elif x == 'l':
+            count -= 1
+        else:
+            print("Invalid Response! Try Again!")
+    elif m == 0:
+
+ 
+ 
+ 
+ 
+ abs()
